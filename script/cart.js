441d10e99372aee053fd96e9082e2980f3e8d272
Marko <marko.dzomba.info@gmail.com>
2023-01-08 23:40:13 +0100

    touch up

--- script/cart.js
+++ script/cart.js
@@ -3,19 +3,19 @@ const products = [
     name: "HTML Course",
     image: "html-icon-logo.png",
     price: 500.99,
-    qtty: 1,
+    qtty: 1
   },
   {
     name: "CSS Course",
     image: "css-icon-logo.png",
     price: 800.5,
-    qtty: 1,
+    qtty: 1
   },
   {
     name: "JS Course",
     image: "js-icon-logo.png",
     price: 2100.0,
-    qtty: 1,
+    qtty: 1
   }
 ];
 //current object formatter
